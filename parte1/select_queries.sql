d9f99bf92e572c4fe30098e8a58ddf2d8a26793b
CamilaCL <cl.lopescamila@gmail.com>
2024-09-30 18:42:58 -0300

    refactor: organização das queries select

--- parte1/select_queries.sql
+++ parte1/select_queries.sql
@@ -61,6 +61,7 @@ SELECT id,
 FROM reserva_mesa;
 
 
+-- retorna todas as mesas em que o checkout é nulo
 SELECT c.nome,
        r.data_hora AS data_reserva,
        rm.id_mesa AS mesa,
@@ -73,14 +74,16 @@ LEFT JOIN mesa m ON rm.id_mesa = m.id
 WHERE rm.checkout IS NULL;
 
 
+-- retorna todas as mesas reservadas a partir de 04/10/2023
 SELECT m.id AS mesa,
        r.data_hora AS data_reserva
 FROM mesa m
 LEFT JOIN reserva_mesa rm ON rm.id_mesa = m.id
 LEFT JOIN reserva r ON r.id = rm.id_reserva
-WHERE r.data_hora > '2023-10-04 19:00:00';
+WHERE r.data_hora > '2023-10-04 00:00:00';
 
 
+-- retorna o histórico de pedidos
 SELECT pe.id,
        pr.nome AS prato,
        pr.preco,
@@ -91,25 +94,7 @@ LEFT JOIN prato pr ON pr.id = pp.id_prato
 LEFT JOIN cliente c ON c.id = pe.id_cliente;
 
 
-SELECT pr.nome AS prato,
-       count(pe.id) AS qtde_pedidos
-FROM pedido pe
-LEFT JOIN pedido_prato pp ON pp.id_pedido = pe.id
-LEFT JOIN prato pr ON pr.id = pp.id_prato
-GROUP BY pr.nome
-ORDER BY qtde_pedidos DESC;
-
-
-SELECT status,
-       count(id) AS qtde_mesas
-FROM mesa
-GROUP BY status;
-
-
-SELECT sum(qtde_assentos) AS qtde_total_assentos
-FROM mesa;
-
-
+-- retorna a quantidade de pedidos por prato
 SELECT p.nome,
        count(pp.id_pedido) AS qtde_pedidos_por_prato
 FROM pedido_prato pp
@@ -118,3 +103,14 @@ GROUP BY p.nome
 ORDER BY qtde_pedidos_por_prato DESC,
          p.nome;
 
+
+-- retorna a quantidade de mesas por status
+SELECT status,
+       count(id) AS qtde_mesas
+FROM mesa
+GROUP BY status;
+
+
+-- retorna a quantidade totald e assentos do restaurante
+SELECT sum(qtde_assentos) AS qtde_total_assentos
+FROM mesa;
